e7935696d5fab868b3c97944a47395fde906ea8b
MkharzomMAL <kharzom.mohammed.mal@gmail.com>
2023-05-29 15:02:32 +0800

    docker init

--- index.php
+++ index.php
@@ -9,9 +9,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="./src/css/style.css">
     <title>BTSB</title>
 </head>
 <body>
-    
+    <header>
+
+    </header>
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-md-6 col-sm-8 m-auto">
+                <form method="POST" action="login.php">
+                    <input class="form-control" type="text" name="user_name" id="">
+                    <input class="form-control" type="email" name="email" id="">
+                    <a class="btn btn-primary">Login</a>
+                </form>
+            </div>
+        </div>
+        
+    </div>
+    <footer>
+
+    </footer>
 </body>
 </html>
